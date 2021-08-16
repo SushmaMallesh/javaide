97849a300ad15c02dfd17513f813e6de68164632
sushmamallesh <aishusushma57@gmail.com>
2021-08-16 17:47:53 +0530

    first commit

---
+++ inheritancee/src/inheritancee/xworks/java/single_inheritance/Bird.java
@@ -0,0 +1,20 @@
+package inheritancee.xworks.java.single_inheritance;
+
+public class Bird {
+	void featherFluffing()
+
+	{
+		System.out.println("birds have feature fluffing");
+	
+		
+	}
+	void dilatingEyes()
+	{
+		System.out.println("birds have dialating eyes");
+	}
+	void biting()
+	{
+		System.out.println("birds are biting");
+
+}
+}
